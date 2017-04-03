1055a7ba7b7588ce531c3506b6c0c68e83583586
MAICON SAUL FARIA <maiconsaulfaria@gmail.com>
2017-04-03 17:43:59 -0300

    Add files via upload
    correct bugs

--- create_geo.py
+++ create_geo.py
@@ -9,14 +9,17 @@ parser.add_argument('--LY', dest='LY', default=20.0, help='comp. domain Y size',
 parser.add_argument('--n_low', dest='n_low', default=1.44, help='Low refractive index', type=float)
 parser.add_argument('--n_high', dest='n_high', default=3.45, help='High refractive index', type=float)
 parser.add_argument('-r', dest='resolution', default=0.050, help='resolution', type=float)
+parser.add_argument('--in_wv', dest='in_wv', default=True, help='Creates Input Waveguide if True', type=float)
 parser.add_argument('--x_in', dest='x_in', default=0.0, help='Input wg X position (left corner)', type=float)
 parser.add_argument('--y_in', dest='y_in', default=9.5, help='Input wg Y position (botton corner)', type=float)
 parser.add_argument('--lx_in', dest='lx_in', default=10.0, help='Input wg X length', type=float)
 parser.add_argument('--ly_in', dest='ly_in', default=0.450, help='Input wg Y length', type=float)
+parser.add_argument('--out_wv', dest='out_wv', default=True, help='Creates Output Waveguide if True', type=float)
 parser.add_argument('--x_out', dest='x_out', default=10.0, help='Output wg X position (left corner)', type=float)
 parser.add_argument('--y_out', dest='y_out', default=9.5, help='Output wg Y position (botton corner)', type=float)
 parser.add_argument('--lx_out', dest='lx_out', default=10.0, help='Output wg X length', type=float)
 parser.add_argument('--ly_out', dest='ly_out', default=0.450, help='Output wg Y length', type=float)
+parser.add_argument('--dev', dest='dev', default=True, help='Creates Device if True', type=float)
 parser.add_argument('--x_dev', dest='x_dev', default=7.5, help='Device X position (left corner)', type=float)
 parser.add_argument('--y_dev', dest='y_dev', default=7.5, help='Device Y position (botton corner)', type=float)
 parser.add_argument('--lx_dev', dest='lx_dev', default=5.0, help='Device X length', type=float)
@@ -45,6 +48,9 @@ x_dev= input.x_dev
 y_dev= input.y_dev
 lx_dev=input.lx_dev
 ly_dev=input.ly_dev
+in_wv=input.in_wv
+out_wv=input.out_wv
+dev=input.dev
 
 device_file=input.device_file
 index_file=input.index_file
@@ -71,11 +77,14 @@ def block(x,y,lx,ly,n):
     domain[x:x+lx ,y:y+ly] = n 
 
 #Waveguide input
-block(x_in,y_in,lx_in,ly_in,n_high)
+if in_wv:
+   block(x_in,y_in,lx_in,ly_in,n_high)
 #Waveguide output
-block(x_out,y_out,lx_out,ly_out,n_high)
+if out_wv:
+   block(x_out,y_out,lx_out,ly_out,n_high)
 #Device
-block(x_dev,y_dev,lx_dev,ly_dev,subarray)
+if dev:
+   block(x_dev,y_dev,lx_dev,ly_dev,subarray)
     
 h5f = h5py.File(index_file, 'w')
 h5f.create_dataset('index', data=domain)
